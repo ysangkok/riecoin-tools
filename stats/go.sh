8a825cb05ea20c70b6e99b04c4b726a1a234e040
Janus <ysangkok@gmail.com>
2014-03-24 01:04:42 +0100

    add stats

---
+++ stats/go.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+cd `dirname ${SCRIPT_PATH}`
+
+cd out
+rm *
+../stats.py
+
+BEGIN=$(grep -nm 1 "Days,Value,FirstUsed,LastReceived,LastSpent,TotalSent,TotalReceived,TransactionCount,PublicKeyAddress" < stats.csv | cut -d: -f1)
+END=$(grep -nm 1 "Summary statistics for 1 months." < stats.csv | cut -d: -f1)
+sed -n $BEGIN,$(expr $END - 1)p < stats.csv > days_value.csv
+A='python3 -c "import sys,csv; csv.writer(sys.stdout).writerows(zip(*csv.reader(sys.stdin)))"'
+grep -A 3 "^Summary statistics for 1 months." < stats.csv | tail -n2 | eval $A > summary.csv # table
+grep -A 3 "^Bitcoin Public Key Address Distribution Count by balance." < stats.csv | tail -n2 | eval $A > address_balance_threshold_count.csv # pie chart
+sed -i '1s/.*/threshold,count/' address_balance_threshold_count.csv
+sed -i '2d' address_balance_threshold_count.csv
+grep -A 3 "^Bitcoin Public Key Address Distribution Total Value by balance." < stats.csv | tail -n2 | eval $A > average_assets_by_balance.csv # pie chart
+sed -i '1s/.*/threshold,count/' average_assets_by_balance.csv
+grep -A 11 "^Bitcoin value distribution based on age." < stats.csv | tail -n +2 > total_assets_and_address_count_by_age.csv # bar chart
+
+rm ~/sdb/www/stat_results/*
+cp * ~/sdb/www/stat_results/
---
+++ stats/stats.py
@@ -0,0 +1,22 @@
+#!/home/ubuntu/sdb/block/bin/python3
+import pexpect
+
+with open("logfile.txt","wb") as f:
+ p = pexpect.spawn("nice ../blockchain.out ../../riecoindata/blocks", timeout=3000, logfile=f)
+ p.expect("exit, quit, or bye    : Will exit this tool")
+ p.sendline("statistics")
+ p.expect("\\*\\*\\* WARNING : This will consume an enormous amount of memory! \\*\\*\\*")
+ p.sendline("analyze")
+ p.expect("Transaction Input Signature Analysis set to: true")
+ p.sendline("export")
+ p.expect("Export Transactions set to: true")
+ p.sendline("record_addresses")
+ p.expect("record_addresses set to true")
+ p.sendline("scan")
+ p.expect("To gather statistics so you can ouput balances of individual addresses, you must execute the 'statistics' command prior to running the process command.")
+ p.sendline("process")
+ p.expect("Saving statistics to file 'stats.csv")
+ p.sendline("top_balance 1000")
+ p.sendline("zombie 30")
+ p.sendline("bye")
+ p.expect(pexpect.EOF)
